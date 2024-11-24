924016976f973d92b61c68ca227f83dae0bd56ce
deepoa <deepeaaa45@gmail.com>
2024-11-24 12:30:39 +0530

    project initiate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RegistrationForm from './Component/Form';
+
+function App() {
+  return (
+    <div className="App">
+     <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
